d81589866db51eb7e3121e09b75440d343d91c77
Rayan <mrayan2525@gmail.com>
2026-03-10 21:31:51 +0000

    Prefix Sums - Sum and L R Range

---
+++ Arrays_and_Strings/Prefix_Sums.py
@@ -0,0 +1,37 @@
+"""
+Prefix Sum is a technique where you precompure cumulative sums of an array
+Instead of summing elements everytime, you build a prefix array once and reuse it
+The first type is the Prefix Sum that stores the cumulative sum of elements
+Range sum of an array adds numbers from L to R pointers of the original array by subtracting from the prefix array
+"""
+
+class Solution:
+    def runningSum(self, nums):
+        prefix = [0] * len(nums) #Duplicating with all 0s
+        prefix[0] = nums[0] #Adding 0 to i results in the same number
+
+        for i in range(1, len(nums)): #Start from 1 because 0 is pre-initialised
+            prefix[i] = prefix[i-1] + nums[i] #Current sum = previous sum + current num
+
+        return prefix
+    
+sol1 = Solution()
+print(sol1.runningSum(nums=[2, 4, 1, 7, 3]))
+
+class Solution2:
+    def rangeSum(self, nums2, L, R):
+        range_prefix = [0] * len(nums2)
+        range_prefix[0] = nums2[0]
+
+        for i in range(1, len(nums2)):
+            range_prefix[i] = range_prefix[i-1] + nums2[i]
+
+        if L == 0:
+            range_sum = range_prefix[R] #No need to subtract L if L is first element
+        else:
+            range_sum = range_prefix[R] - range_prefix[L-1] # Subtracting by L-1 because R includes sums before L
+        
+        return range_sum
+
+sol2 = Solution2()
+print(sol2.rangeSum([2, 5, 1, 7, 2], 2, 4))
