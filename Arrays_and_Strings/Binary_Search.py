8530231e951abd288977ee7780152a8a1baf32dc
Rayan <mrayan2525@gmail.com>
2026-03-15 15:44:35 +0000

    Binary Search to find target value

---
+++ Arrays_and_Strings/Binary_Search.py
@@ -0,0 +1,29 @@
+"""
+Binary Search is a search algorithm used to find a target value in a sorted array
+Instead of checking every element we find the middle element and adjust L and R pointers accordingly
+This significantly improves time complexity as it eliminates half the possibilities each step
+In this example a target number is found by moving left, right and middle pointers"""
+
+class Solution:
+    def binary_search(self, arr, target):
+        left = 0
+        right = len(arr) - 1
+
+        while left <= right: #Loop while there is still a valid search range
+            mid = left + ((right - left) // 2) #Calculation of mid index to avoid integer overflow
+            #Mid auto adjusts every iteration of the while loop
+
+            if arr[mid] == target:
+                return True
+            
+            elif arr[mid] > target:
+                right = mid - 1 #search to the left of the current mid value
+
+            else:
+                left = mid + 1 #Search to the right of the current mid value
+        
+        return False
+
+
+sol = Solution()
+print(sol.binary_search(arr=[-3, -2, -1, 0, 1, 2, 3], target = 1))
