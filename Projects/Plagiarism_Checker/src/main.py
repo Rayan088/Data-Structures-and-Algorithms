a45eb125fbbb41e2943ccc8c98a4df29203d24a0
Rayan <mrayan2525@gmail.com>
2026-03-20 10:37:14 +0000

    Added commentary

--- Projects/Plagiarism_Checker/src/main.py
+++ Projects/Plagiarism_Checker/src/main.py
@@ -15,12 +15,20 @@ def read_file(file):
 doc1 = read_file("Projects/Plagiarism_Checker/data/sample1.txt")
 doc2 = read_file("Projects/Plagiarism_Checker/data/sample2.txt")
 
+#Reads both files into strings
+
 tokens1 = remove_stop_words(tokenisation(clean_text(doc1)))
 tokens2 = remove_stop_words(tokenisation(clean_text(doc2)))
+
+#Removes stop words
 
 sim = Similarity()
 score = sim.jaccard_similarity(tokens1, tokens2)
 
+#Calculates similarity through Jaccard Similarity
+
 kgram = Kgrams()
 kgrams1 = kgram.generate_kgrams(tokens1, 3)
 kgrams2 = kgram.generate_kgrams(tokens2, 3)
+
+#Generates 3 grams for each document
