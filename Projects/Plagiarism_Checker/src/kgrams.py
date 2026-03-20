3f9e671f35a7024723ba53625d3712a48af34287
Rayan <mrayan2525@gmail.com>
2026-03-20 10:14:11 +0000

    Generates k_grams of length k

---
+++ Projects/Plagiarism_Checker/src/kgrams.py
@@ -0,0 +1,11 @@
+class kgrams:
+    def generate_kgrams(self, tokens, k):
+        k_grams = []
+
+        for i in range(len(tokens) - k + 1): #Looping to avoid going out of bounds
+            phrase = " ".join(tokens[i:i+k]) #Converts list into string
+            k_grams.append(phrase)
+
+        return k_grams
+    
+#Function that splits tokens into kgrams of length k
