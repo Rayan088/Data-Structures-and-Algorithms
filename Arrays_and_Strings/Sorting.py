bdba884cab0e34bedb424df757809d1e6cf4f979
Rayan <mrayan2525@gmail.com>
2026-03-25 18:45:05 +0000

    selection sort implemented

--- Arrays_and_Strings/Sorting.py
+++ Arrays_and_Strings/Sorting.py
@@ -2,7 +2,9 @@
 Sorting is the process of arranging a collection of elements in a specific order
 This order can be from smallest to largest or vice verca
 The following example is the Bubble Sort algorithm which swaps elements until they are in the correct order
-Bubble sort is inefficient for large data sets, compared to other sorting algorithms"""
+Bubble sort is inefficient for large data sets, compared to other sorting algorithms
+Selection sort algorithm sorts an array by repeatedly picking the smallest element from the unsorted part and placing it in the correct position
+"""
 
 class Solution:
     def bubble_sort(self, arr):
@@ -20,7 +22,23 @@ class Solution:
                 break
 
         return arr
+
+    def selection_sort(self, arr):
+        n = len(arr) #Length of array
+
+        for i in range(n):
+            min_index = i #Assume first unsorted element is the minimum
+
+            for j in range(i+1, n):
+                if arr[j] < arr[min_index]: #Find the true minimum
+                    min_index = j
+
+        arr[i], arr[min_index] = arr[min_index], arr[i] #Swap found minimum with unsorted element
+
+        return arr
     
 arr = [5, 2, 9, 1, 5, 6]
 sol1 = Solution()
-print(sol1.bubble_sort(arr))
+
+print(f"Bubble Sort: {sol1.bubble_sort(arr)}")
+print(f"Selection Sort: {sol1.selection_sort(arr)}")
