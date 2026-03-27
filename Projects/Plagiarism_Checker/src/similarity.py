355c4482384cf051fb9bc6047c01d0993bef003a
Rayan <mrayan2525@gmail.com>
2026-03-27 21:33:57 +0000

    Fixed jaccard score representation

--- Projects/Plagiarism_Checker/src/similarity.py
+++ Projects/Plagiarism_Checker/src/similarity.py
@@ -23,15 +23,15 @@ class Similarity:
         calculation = format(calculation, "0.2g") #Rounded to 2 decimal points
         calculation = float(calculation)
 
-        if union == 0 or intersection == 0:
-            return 0
-        elif  0.0 < calculation < 0.1:
+        if union == 0:
+            return f"Jaccard score: ({calculation}) signifies No Similarity"
+        elif  0.0 < calculation <= 0.1:
             return f"Jaccard score: ({calculation}) signifies Very Low Similarity"
-        elif 0.1 < calculation < 0.3:
+        elif 0.1 < calculation <= 0.3:
             return f"Jaccard score: ({calculation}) signifies Low Similarity"
-        elif 0.3 < calculation < 0.5:
+        elif 0.3 < calculation <= 0.5:
             return f"Jaccard Score ({calculation}) signifies Moderate Similarity"
-        elif 0.5 < calculation < 0.7:
+        elif 0.5 < calculation <= 0.7:
             return f"Jaccard Score ({calculation}) signifies High Similarity"
         else:
             return f"Jaccard Score ({calculation}) signifies Very High Similarity"
