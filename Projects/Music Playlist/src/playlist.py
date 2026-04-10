c5f839e8759be674ef42ee782f7ebaa056bf64d3
Rayan <mrayan2525@gmail.com>
2026-04-10 17:31:43 +0100

    Implemented shuffle function and appropriate returns

--- Projects/Music Playlist/src/playlist.py
+++ Projects/Music Playlist/src/playlist.py
@@ -1,5 +1,7 @@
 from song_node import SongNode
 
+import random
+
 class Playlist:
     def __init__(self, head=None, tail=None, curr=None):
         self.head = head
@@ -13,11 +15,12 @@ class Playlist:
 
         if self.head is None:
             self.head = self.tail = self.curr = new_node
-            return
+            return f"Now playing {self.curr}"
 
         self.tail.next = new_node
         new_node.prev = self.tail
         self.tail = new_node
+        return f"New song added to end of list (Title: {new_node.title}, Artist: {new_node.artist}, Duration {new_node.duration} seconds, Genre: {new_node.genre})"
 
     #Method which adds song to end of linked list
 
@@ -29,20 +32,53 @@ class Playlist:
     def prev_song(self):
         if self.curr and self.curr.prev:
             self.curr = self.curr.prev
-        else:
-            print("Already at the start of the playlist")
+            return f"Now playing {self.curr}"
+        return f"Already at the start of the playlist"
 
     #Method which moves to previous song
 
     def next_song(self):
         if self.curr and self.curr.next:
             self.curr = self.curr.next
-        else:
-            print("Already at the end of the playlist")
+            return f"Now playing {self.curr}"
+        return f"Already at the end of the playlist"
         
     #Method which moves to next song
 
     def shuffle(self):
-        pass
+        curr = self.head
+
+        if curr is None:
+            return "Platlist is empty"
+        
+        nodes = []
+        while curr:
+            nodes.append(curr)
+            curr = curr.next
+
+        random.shuffle(nodes)
+
+        self.head = nodes[0]
+        self.head.prev = None
+
+        for i in range(1, len(nodes)):
+            nodes[i-1].next = nodes[i]
+            nodes[i].prev = nodes[i-1]
+
+        self.tail = nodes[len(nodes) - 1]
+        self.tail.next = None
+        
+        self.curr = self.head
+        return f"Playlist has been shuffled! Now playing {self.curr}"
 
     #Method which shuffles songs
+
+
+pl = Playlist()
+
+print(pl.add_song("Blinding Lights", "The Weeknd", 200, "Pop"))
+print(pl.add_song("Bohemian Rhapsody", "Queen", 354, "Rock"))
+print(pl.add_song("Shape of You", "Ed Sheeran", 240, "Pop"))
+print(pl.add_song("Lose Yourself", "Eminem", 326, "Hip-Hop"))
+print(pl.shuffle())
+print(pl.next_song())
