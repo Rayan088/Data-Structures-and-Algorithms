bf8dd37e9500357562ada43527fcf21984760df0
Rayan <mrayan2525@gmail.com>
2026-04-11 18:46:33 +0100

    Added importing songs functionality

--- Projects/Music Playlist/src/playlist.py
+++ Projects/Music Playlist/src/playlist.py
@@ -83,3 +83,15 @@ class Playlist:
         return f"Playlist has been shuffled! Now playing {self.curr}\n"
 
     #Method which shuffles songs
+
+    def import_songs(self, csv_file):
+        songs = SongNode.import_songs(csv_file)
+        if not songs:
+            return "No songs were imported due to an error"
+        
+        for song in songs:
+            self.add_song(song.title, song.artist, song.duration, song.genre)
+
+        return f"Successfully imported {len(songs)} from CSV file"
+    
+    #Method which adds songs from csv file
--- Projects/Music Playlist/src/song_node.py
+++ Projects/Music Playlist/src/song_node.py
@@ -1,3 +1,5 @@
+import csv
+
 class SongNode:
     def __init__(self, title, artist, duration, genre, next=None, prev=None):
         self.title = title
@@ -13,3 +15,20 @@ class SongNode:
         return f"{self.title} - {self.artist} ({self.duration})"
     
     #Str method for user friendly representation
+
+    def import_songs(self, csv_file):
+        try:
+            with open(csv_file, mode="r", newline='', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                for row in reader:
+                    title = row['Title']
+                    artist = row['Artist']
+                    duration = row['Duration']
+                    genre = row['Genre']
+                    self.add_song(title, artist, duration, genre)
+            return f"Successfully imported songs from {csv_file}."
+        
+        except Exception as e:
+            return f"Error importing songs: {e}"
+
+    #Method to import songs from csv file
