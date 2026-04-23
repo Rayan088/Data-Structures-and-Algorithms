e1a2c517dd6151d7e072f282f4c7d4d9d61b2779
Rayan <mrayan2525@gmail.com>
2026-04-23 19:30:15 +0100

    Creted GUI skelaton

--- Projects/Music_Playlist/src/GUI.py
+++ Projects/Music_Playlist/src/GUI.py
@@ -10,16 +10,56 @@ class App:
         self.root.geometry("1000x700")
         self.root.title("Music Playlist")
 
+        self.pl = Playlist()
+        self.graphs = Graphs(self.pl)
+
         self.main_frame = tk.Frame(root)
         self.stats_frame = tk.Frame(root)
         self.graph_frame = tk.Frame(root)
 
-        self.show_frame()
+        self.create_main_frame()
+        self.create_stats_frame()
+        self.create_graphs_frame()
+
+        self.show_frame(self.main_frame)
 
     def show_frame(self, frame):
         for f in (self.main_frame, self.stats_frame, self.graph_frame):
             f.pack_forget()
         frame.pack(fill="both", expand=True)
+
+    def create_main_frame(self):
+        pass
+
+    def create_stats_frame(self):
+        pass
+
+    def create_graphs_frame(self):
+        pass
+
+    def add_song(self, song):
+        pass
+
+    def remove_song(self, song):
+        pass
+
+    def next_song(self):
+        pass
+
+    def previous_song(self):
+        pass
+
+    def shuffle(self):
+        pass
+
+    def stats(self):
+        pass
+
+    def visualisations(self):
+        pass
+
+    def import_from_csv(self):
+        pass
 
 root = tk.Tk()
 app = App(root)
