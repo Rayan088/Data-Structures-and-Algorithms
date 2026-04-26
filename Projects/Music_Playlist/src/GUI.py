dd0c02857d7093b3183d78d46fedd8b61a356320
Rayan <mrayan2525@gmail.com>
2026-04-26 22:13:58 +0100

    Implemented graphs in visualisations

--- Projects/Music_Playlist/src/GUI.py
+++ Projects/Music_Playlist/src/GUI.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import tkinter.messagebox as messagebox
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
 
 from src.playlist import Playlist
 from visualisations.graphs import Graphs
@@ -10,6 +12,8 @@ class App:
         self.root.geometry("1000x700")
         self.root.title("Music Playlist")
         self.root.configure(bg="lightblue")
+
+        self.root.protocol("WM_DELETE_WINDOW", self.on_close)
 
         self.pl = Playlist()
         self.graphs = Graphs(self.pl)
@@ -23,6 +27,11 @@ class App:
         self.create_graphs_frame()
 
         self.show_frame(self.main_frame)
+
+    def on_close(self):
+        plt.close("all")
+        self.root.quit()
+        self.root.destroy()
 
     def show_frame(self, frame):
         for f in (self.main_frame, self.stats_frame, self.graph_frame):
@@ -64,7 +73,7 @@ class App:
         for i in range(6):
             self.stats_frame.grid_columnconfigure(i, weight=1)
 
-        tk.Label(self.stats_frame, text="Stats", font=("Arial", 28, "bold"), bg="#120A38", fg="#FFFFFF").grid(row=0, column=0, columnspan=6, pady=10, sticky="nsew")
+        tk.Label(self.stats_frame, text="Stats", font=("Arial", 28, "bold"), bg="#120A38", fg="#FFFFFF").grid(row=0, column=0, columnspan=6, pady=10, sticky="nsew")   
         tk.Button(self.stats_frame, text="Back", command=lambda: self.show_frame(self.main_frame), bg="#22145C", fg="#FFFFFF").grid(row=1, column=0, columnspan=6, pady=50)
 
     def create_graphs_frame(self):
@@ -74,7 +83,39 @@ class App:
             self.graph_frame.grid_columnconfigure(i, weight=1)
 
         tk.Label(self.graph_frame, text="Visualisations", font=("Arial", 28, "bold"), bg="#120A38", fg="#FFFFFF").grid(row=0, column=0, columnspan=6, pady=10, sticky="snew")
-        tk.Button(self.graph_frame, text="Back", command=lambda: self.show_frame(self.main_frame), bg="#22145C", fg="#FFFFFF").grid(row=1, column=0, columnspan=6, pady=50)
+        
+        fig1 = self.graphs.song_durations()
+        fig2 = self.graphs.songs_per_genre()
+        fig3 = self.graphs.common_title_words()
+
+        canvas1 = FigureCanvasTkAgg(fig1, master=self.graph_frame)
+        canvas1.draw()
+        canvas1.get_tk_widget().grid(row=1, column=0, columnspan=6, pady=10)
+
+        canvas2 = FigureCanvasTkAgg(fig2, master=self.graph_frame)
+        canvas2.draw()
+        canvas2.get_tk_widget().grid(row=2, column=0, columnspan=6, pady=10)
+
+        canvas3 = FigureCanvasTkAgg(fig3, master=self.graph_frame)
+        canvas3.draw()
+        canvas3.get_tk_widget().grid(row=3, column=0, columnspan=6, pady=10)
+
+        tk.Button(self.graph_frame, text="Back", command=lambda: self.show_frame(self.main_frame), bg="#22145C", fg="#FFFFFF").grid(row=4, column=0, columnspan=6, pady=50)
+
+    def load_graphs(self):
+        for widget in self.graph_frame.winfo_children():
+            if widget not in self.graph_frame.grid_slaves(row=0) + self.graph_frame.grid_slaves(row=5):
+                widget.destroy()
+
+        fig1 = self.graphs.song_durations()
+        fig2 = self.graphs.songs_per_genre()
+        fig3 = self.graphs.common_title_words()
+
+        for i, fig, in enumerate([fig1, fig2, fig3], start=1):
+            canvas = FigureCanvasTkAgg(fig, master=self.graph_frame)
+            canvas.draw()
+            canvas.get_tk_widget().grid(row=i, column=0, columnspan=6, pady=10)
+
 
     def add_song(self):
         self.popup = tk.Toplevel(self.root)
@@ -173,6 +214,7 @@ class App:
         self.show_frame(self.stats_frame)
 
     def visualisations(self):
+        self.load_graphs()
         self.show_frame(self.graph_frame)
 
 root = tk.Tk()
