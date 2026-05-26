751f9d00ea123a23b9e7da4a6da03c6a3c746b2b
Rayan <mrayan2525@gmail.com>
2026-05-26 22:13:32 +0100

    Removed bugs and edge cases

--- Projects/Hierarchy_Manager/src/operations.py
+++ Projects/Hierarchy_Manager/src/operations.py
@@ -1,35 +1,62 @@
 from structure import Employee
 
 class Operations:
-    def insertEmployee(self, position):
-        newEmployee = Employee(position)
-
-        self.children.append(newEmployee)
-        print(f"Employee added successfully. (ID: {Employee.Id})")
- 
-        return newEmployee
+    def __init__(self, root=None):
+        self.root = root
     
+    # Initializer method
+
+    def insert_employee(self, name, position, manager_id=None):
+        if self.root is None:
+            self.root = Employee(name, position)
+
+            return self.root
+        
+        # Inserting head employee
+        
+        manager = self.search_employee(manager_id)
+
+        if manager is None:
+            print("Manager not found")
+            return
+        
+        # Checking for manager
+        
+        employee = Employee(name, position, manager)
+
+        manager.children.append(employee)
+
+        return employee
+ 
     # Method to insert employee
 
-    def searchEmployee(self, employee_Id):
-        if self.Id == employee_Id:
-            return self
+    def search_employee(self, employee_id):
+        if self.root is None:
+            return None
         
-        for child in self.children:
-            result = child.searchEmployee(employee_Id)
+        if self.root.employee_id == employee_id:
+            return self.root
+        
+        stack = [self.root]
 
-            if result:
-                return result
-            
-        return None
+        while stack:
+            current = stack.pop()
+
+            for child in current.children:
+                if child.employee_id == employee_id:
+                    return child
+
+                stack.append(child)
+
+        return None    
     
     # Method to search for employee
     
-    def updateEmployee(self, employee_Id, name=None, position=None):
-        employee = self.searchEmployee(employee_Id)
+    def update_employee(self, employee_id, name=None, position=None):
+        employee = self.search_employee(employee_id)
 
         if employee is None:
-            print(f"No employee found with ID {employee_Id}")
+            print(f"No employee found with ID {employee_id}")
             return
         
         if name is not None:
@@ -38,23 +65,31 @@ class Operations:
         if position is not None:
             employee.position = position
 
-        print(f"Employee added successfully (Employee ID {employee_Id})")
+        print(f"Employee updated successfully (Employee ID {employee_id})")
 
     # Method to update employee
 
-    def delete_Employee(self, employee_Id):
-        for child in self.children:
-            if child.id == employee_Id:
-                self.children.remove(child)
+    def delete_employee(self, employee_id):
+        if self.root is None:
+            return None
+        
+        if self.root.employee_id == employee_id:
+            print("Cannot delete root emplyoee")
+            return False
+        
+        stack = [self.root]
 
-                print(f"Employee delted successfully (Employee ID {employee_Id})")
-                return True
-            
-            deleted = child.deleteEmployee(employee_Id)
+        while stack:
+            current = stack.pop()
 
-            if deleted:
-                return True
-            
+            for child in current.children:
+                if child.employee_id == employee_id:
+                    current.children.remove(child)
+                    return True
+                
+                stack.append(child)
+
         return False
+
 
     # Method to delete employee
--- Projects/Hierarchy_Manager/src/structure.py
+++ Projects/Hierarchy_Manager/src/structure.py
@@ -1,15 +1,24 @@
 import random
 
 class Employee:
-    def __init__(self, position, head, Id=None):
-        self.head = head
+    used_ids = set()
+
+    def __init__(self, name, position, manager=None, employee_id=None):
+        self.name = name
         self.position = position
+        self.manager = manager
         self.children = []
 
-        if Id is None:
-            randomId = str(random.randint(0, 9999)).zfill(4)
-            self.Id = randomId
-        else:
-            self.Id = Id
+        if employee_id is None:
+            while True:
+                random_id = str(random.randint(0, 9999)).zfill(4)
 
+                if random_id not in Employee.used_ids:
+                    Employee.used_ids.add(random_id)
+                    self.employee_id = random_id
+                    break
+        else:
+            self.employee_id = employee_id
+            Employee.used_ids.add(employee_id)
+            
     #Initialiser method
