19f24cea76d2379dfe726b68fc651a048fed9d00
Rayan <mrayan2525@gmail.com>
2026-06-09 20:55:53 +0100

    Basic frontend setup

---
+++ Projects/Plagiarism_Checker/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [file1, setFile1] = useState(null);
+
+
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+export default App;
