e19bb073503c312a0ffe2a462917de61303ffe8e
Rayan <mrayan2525@gmail.com>
2026-06-16 19:20:42 +0100

    Successful trades class

---
+++ Projects/Trade_Order_Matching_Engine/backend/engine/trade.py
@@ -0,0 +1,31 @@
+import time
+
+# Class for successful trades
+
+class Trade:
+    trade_counter = 1
+
+    def __init__(self, buy_order, sell_order, price, quantity, side):
+        self.trade_id = Trade.trade_counter
+
+        Trade.trade_counter += 1
+
+        self.buy_order = buy_order
+        self.sell_order = sell_order
+        self.price = price
+        self.quantity = quantity
+        self.side = side
+
+        self.time = time.time()
+
+    def get_trade_data(self):
+        return {
+            "trade_id": self.trade_id,
+            "buy_order": self.buy_order,
+            "sell_order": self.sell_order,
+            "price": self.price,
+            "quantity": self.quantity,
+            "time": self.time
+        }
+    
+    # Method returning trade data in a string
