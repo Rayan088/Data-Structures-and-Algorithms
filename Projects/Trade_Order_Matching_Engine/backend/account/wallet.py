a9d3b98f4d1fed55d4699c4b4c5d26651b01efdf
Rayan <mrayan2525@gmail.com>
2026-06-18 11:31:12 +0100

    User wallet file

---
+++ Projects/Trade_Order_Matching_Engine/backend/account/wallet.py
@@ -0,0 +1,43 @@
+class Wallet:
+    def __init__(self):
+        self.btc = 1.0
+        self.cash = 0.0
+
+    def buy_btc(self, price, quantity):
+        cost = price * quantity
+
+        if self.cash >= cost:
+            self.cash -= cost
+            self.btc += quantity
+
+            return True
+        
+        return False
+    
+    # Method calculating funds after purchase
+    
+    def sell_btc(self, price, quantity):
+        if self.btc >= quantity:
+            revenue = price * quantity
+
+            self.btc -= quantity
+            self.cash += revenue
+
+            return True
+        
+        return False
+    
+    # Method calculating funds after sale
+
+    def get_total_value(self, btc_price):
+        return self.cash + (self.btc * btc_price)
+    
+    # Method calculating total portfolio value
+
+    def get_balance(self):
+        return {
+            "BTC": self.btc,
+            "USD": self.cash
+        }
+    
+    # Method returning current balance
