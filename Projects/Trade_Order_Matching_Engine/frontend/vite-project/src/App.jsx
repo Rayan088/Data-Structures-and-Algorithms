5302b8e7e0cde767d0c891f7718427f8eb45ff53
Rayan <mrayan2525@gmail.com>
2026-06-20 10:59:09 +0100

    Created frontend outline with vite

---
+++ Projects/Trade_Order_Matching_Engine/frontend/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Wallet from './components/Wallet'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <p>BTC Exchange</p>
+      <Wallet />
+    </div>
+  )
+}
+
+export default App
---
+++ Projects/Trade_Order_Matching_Engine/frontend/vite-project/src/api/exchange.js
@@ -0,0 +1,40 @@
+const API = "http://localhost:5000";
+
+export async function getOrderBook() {
+    const res = await fetch(`${API}/orderbook`)
+    return res.json();
+}
+
+export async function getTrades(){
+    const res = await fetch(`${API}/trades`);
+    return res.json();
+}
+
+export async function getUserOrders(){
+    const res = await fetch(`${API}/userorders`);
+    return res.json();
+}
+
+export async function getWallet() {
+    const res = await fetch(`${API}/wallet`)
+    return res.json();
+}
+
+export async function getStats(){
+    const res = await fetch(`${API}/stats`);
+    return res.json();
+}
+
+export async function placeOrder(order){
+    const res = await fetch(
+        `${API}/order`,
+        {
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(order)
+        }
+    );
+    return res.json();
+}
